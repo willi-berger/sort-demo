083f5257b614d525d366859fbafdeb6faa654cc8
willi-berger <willi.berger@gmx.at>
2023-08-22 18:14:21 +0200

    implement demo for selection sort

--- script/main.js
+++ script/main.js
@@ -7,7 +7,6 @@ const SortDemo = function(canvas) {
 	
 	/** @type {CanvasRenderingContext2D} */
     var ctx = canvas.getContext('2d')
-    var timeStep = 500; // In milliseconds
     const CW = ctx.canvas.width;    // 600
     const CH = ctx.canvas.height;   // 400
     const MARGIN_BOTTOM = 20;
@@ -17,7 +16,7 @@ const SortDemo = function(canvas) {
     const N_ITEMS = 50;
     const MAX_VALUE = 100;
 
-    const TIME_STEP_MS = 250;
+    const TIME_STEP_MS = 50;
 
     const baseLineLength = CW - 2 * MARGIN_SIDE;
     const lineFieldheigt = CH - MARGIN_BOTTOM - MARGIN_TOP;
@@ -34,15 +33,27 @@ const SortDemo = function(canvas) {
     const Item = function(value, index) {
         this.value = value;
         this.index = index;
-        this.higlight = 0;
+        this.state = 0;
 
         this.stroke = function(i) {
             ctx.save();
             ctx.beginPath();
             ctx.lineWidth = 3;
-            if (this.higlight == 1) {
-                ctx.strokeStyle = 'green';
+            switch (this.state) {
+                case 'compare':
+                    ctx.strokeStyle = 'yellow';
+                    break;
+                case 'current_min':
+                    ctx.strokeStyle = 'blue';
+                    break;
+                case 'done':
+                    ctx.strokeStyle = 'green';
+                    break;
+                default:
+                    ctx.strokeStyle = 'black';
+                    break;
             }
+
             ctx.moveTo(MARGIN_SIDE + i*itemFieldWidth + itemFieldWidth/2, MARGIN_BOTTOM)
             ctx.lineTo(MARGIN_SIDE + i*itemFieldWidth + itemFieldWidth/2, MARGIN_BOTTOM + this.value / MAX_VALUE * lineFieldheigt)            
             ctx.stroke()
@@ -55,8 +66,10 @@ const SortDemo = function(canvas) {
     ctx.scale(1 , -1);
 	
     // initialize items to sort
+    let min = 10
+    let max = MAX_VALUE - 10
     for(let i = 0; i < N_ITEMS; i++) {
-        this.items.push(new Item(Math.floor(Math.random() * MAX_VALUE), i));
+        this.items.push(new Item(Math.floor(Math.random() * max) + min, i));
     }
     console.log(this.items)
 
@@ -67,6 +80,7 @@ const SortDemo = function(canvas) {
      */
 	this.stroke = function() {
 
+        console.log('stroke')
         // draw items
         this.items.forEach((item, i) => item.stroke(i))
 
@@ -87,14 +101,34 @@ const SortDemo = function(canvas) {
      */
     this.selectionSort = function* (self) {
 
-        for (let i = 0; i < N_ITEMS; i++) {
+        let i = 0;
+        let j = 0;
+        for (i = 0; i < N_ITEMS - 1; i++) {
+            let current_min_index = i;
+            self.items[i].state = 'compare';
 
-            console.dir(`i = ${i}`);
-            self.items[i].higlight = 1;
-            yield "hello";
+            for (j = i + 1; j < N_ITEMS; j++ ) {
+                console.log(`i = ${i} j = ${j}`);
+                self.items[j].state = 'compare';
+                yield {'op': 'compare', 'items': [i, j]}
+
+                if (this.items[j].value < this.items[current_min_index].value) {
+                    console.debug(`new min: index ${j} value ${this.items[j].value}`)
+                    this.items[current_min_index].state = 'default'
+                    current_min_index = j;
+                    this.items[j].state = 'current_min';
+                } else {
+                    this.items[j].state = 'default';
+                }
+            } 
+
+            if (i != current_min_index) {
+                [this.items[i], this.items[current_min_index]] = [this.items[current_min_index], this.items[i]];
+            }
+            this.items[i].state = 'done';
+            yield {'op': 'swap', 'items': [i, j]}
+
         }
-
-        yield 'finished'
     }
 
     /**
@@ -103,14 +137,15 @@ const SortDemo = function(canvas) {
      * @param {Generator} generator 
      */
     this.runAnimation = function(self, generator) {
-        console.log('runAnimation')
+        console.log('animatiionstep step')
         ctx.clearRect(0, 0, CW, CH);
         self.stroke();
 
         let res = generator.next();
-        console.debug('generator res: ' + res)
-
+        let value = res.value;
+        
         if (!res.done) {
+            console.debug(`generator res: op: ${value.op}, items ${value.items}`)
             timerId = setTimeout(self.runAnimation, TIME_STEP_MS, self, generator);
         } else {
             console.info('Finished');
@@ -123,8 +158,6 @@ const SortDemo = function(canvas) {
     }
 	
 }
-
-
 
 $(document).ready(function () {
     var canvas = $("#canvas")[0]
